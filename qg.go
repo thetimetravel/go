00a8541ab3957f0fa7922ea00d4b2995fec31c2b
time <32856645+thetimetravel@users.noreply.github.com>
2020-09-18 17:54:41 +0800

    Update and rename qg to qg.go

--- qg.go
+++ qg.go
@@ -6,14 +6,14 @@ type QG struct {
 	name string
 	age int
 } 
-func init()  {
-	qg:=QG{
-		name: "xq",
-		age:  33,
-	}
-	qg.Show_info()
-	fmt.Println("good:github")
-}
+//func init()  {
+//	qg:=QG{
+//		name: "xq",
+//		age:  33,
+//	}
+//	qg.Show_info()
+//	fmt.Println("good:github")
+//}
 
 func (qg *QG)Show_info()  {
 	fmt.Println("info: name",qg.name," ,age:  ",qg.age)
