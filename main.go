a064678283c2a33fc83997832b8da242b87beac2
time <32856645+thetimetravel@users.noreply.github.com>
2020-09-18 18:02:51 +0800

    Update and rename qg.go to main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package qg
+
+import "fmt"
+
+type QG struct {
+	Name string
+	Age int
+} 
+func init()  {
+	qg:=QG{
+		Name: "wwwwwww",
+		Age:  444,
+	}
+	qg.Show_info()
+	fmt.Println("good:github")
+}
+
+func (qg *QG)Show_info()  {
+	fmt.Println("info: name",qg.Name," ,age:  ",qg.Age)
+}
